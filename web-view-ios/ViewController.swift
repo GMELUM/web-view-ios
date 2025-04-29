4a0fef4b2da6f9994a31d5b3c82bbd9dd4db348e
GMELUM <78602165+GMELUM@users.noreply.github.com>
2025-04-30 00:07:36 +0300

    Initial Commit

---
+++ web-view-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  web-view-ios
+//
+//  Created by Артур Гетьман on 30.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ web-view-iosTests/web_view_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  web_view_iosTests.swift
+//  web-view-iosTests
+//
+//  Created by Артур Гетьман on 30.04.2025.
+//
+
+import Testing
+@testable import web_view_ios
+
+struct web_view_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
